910e6e171232d9fd5548af4383f24734a7a14ebf
NikolayTkachenko <tkachenni@mail.ru>
2024-08-25 02:17:55 +0300

    ViewModels Flow+Coroutines

---
+++ app/src/main/java/com/example/appjetpacktest001/experiments/Test001.kt
@@ -0,0 +1,7 @@
+package com.example.appjetpacktest001.experiments
+
+class Test001 {
+    //inner data class(val Name: String) -> Error
+    //data class(val Name: String) -> Working
+    //inline data class(val Name: String) -> Error
+}
